3cbecd42898eee234e577b312551e77cd9c87373
EggTree124 <robloxgamingindonesia7@gmail.com>
2026-08-06 14:59:48 +0700

    Drainage mechanism

---
+++ scenes/drainage.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var drain_speed := 2
+
+func _on_timer_timeout():
+
+	for zone in get_overlapping_areas():
+
+		if zone.is_in_group("flood_zones"):
+
+			zone.water_level -= drain_speed
+			zone.water_level = max(zone.water_level, 0)

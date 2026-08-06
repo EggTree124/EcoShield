4436c07ac73b6a8ba544b9074ef7883ba507cef3
EggTree124 <robloxgamingindonesia7@gmail.com>
2026-08-06 14:37:30 +0700

    Sewerage

--- flood_zone.gd
+++ flood_zone.gd
@@ -6,19 +6,16 @@ extends Area2D
 func _on_timer_timeout() -> void:
 	water_level += flood_speed
 	water_level = clamp(water_level, 0, max_water)
-	print("Water Level:", water_level)
 	
 	for area in get_overlapping_areas():
 		if area.is_in_group("houses"):
 			area.current_water_level = water_level
 func _on_area_entered(area) -> void:
 	if area.is_in_group("houses"):
-		print(area.house_name, " entered flood zone")
 		area.in_flood_zone = true
 		area.current_water_level = water_level
 
 func _on_area_exited(area) -> void:
 	if area.is_in_group("houses"):
-		print(area.house_name, " left flood zone")
 		area.in_flood_zone = false
 		area.current_water_level = 0
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,5 +1,17 @@
 extends Node2D
-
+@export var drainage_scene : PackedScene
+var build_mode := false
 func _on_timer_timeout() -> void:
 	for house in get_tree().get_nodes_in_group("houses"):
 		house.generate_waste()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("build_drainage"):
+		build_mode = true
+
+	if build_mode and event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var drainage = drainage_scene.instantiate()
+			drainage.global_position = get_global_mouse_position()
+			add_child(drainage)
+			build_mode = false
